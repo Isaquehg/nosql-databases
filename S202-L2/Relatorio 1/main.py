71f54b49491ec35e4ac14bed79d197fa958ed0b9
Isaquehg <isaqueh.goncalves@gmail.com>
2023-02-18 17:04:25 -0300

    relatorio 1

--- S202-L2/Relatorio 1/main.py
+++ S202-L2/Relatorio 1/main.py
@@ -1,7 +1,7 @@
 from elefante import Elefante
 
 nome = input("Nome do elefante: ")
-idade = input("Idade do elefante: ")
+idade = int(input("Idade do elefante: "))
 especie = input("Especie do elefante: ")
 cor = input("Cor do elefante: ")
 tamanho = input("Tamanho do elefante: ")
@@ -9,15 +9,16 @@ tamanho = input("Tamanho do elefante: ")
 e1 = Elefante(nome, idade, especie, cor, "PpPaAaHhHh", tamanho)
 e1.emitir_som()
 
-if(especie == "Africano" & idade < 10):
+if((especie == "Africano") & (idade < 10)):
     e1.som = "Paaah"
     e1.tamanho = "Pequeno"
-    e1.cor = ""
-elif(especie == "Africano" & idade >= 10):
+    e1.cor = "Beje"
+elif((especie == "Africano") & (idade >= 10)):
     e1.som = "PAHHHHHH"
     e1.tamanho = "Grande"
-    e1.cor = ""
+    e1.cor = "Cinza"
 
-print("Novo Som: " + e1.emitir_som())
+print("Novo Som: ")
+e1.emitir_som()
 print(f"Novo Tamanho: {e1.tamanho}")
 print(f"Nova Cor: {e1.cor}")
